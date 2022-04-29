11717aff948abd56b9968c9f0ba8b51c77698f37
upuna <ukeerawella@gmail.com>
2022-04-29 22:50:51 +0530

    Initial commit

---
+++ src/main/java/com/upuna/jwt/SpringbootAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package com.upuna.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAuthJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upuna/jwt/SpringbootAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upuna.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
